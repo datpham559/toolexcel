0d6db14b7c23209abd0e5ccf0717c0ae71bca586
datpt <datpt@softdreams.vn>
2022-12-19 20:13:49 +0700

    upt

--- src/main/java/com/softdreams/excel/repository/SyntheticRepository.java
+++ src/main/java/com/softdreams/excel/repository/SyntheticRepository.java
@@ -2,16 +2,18 @@ package com.softdreams.excel.repository;
 
 import com.softdreams.excel.domain.Synthetic;
 import com.softdreams.excel.repository.custom.SyntheticRepositoryCustom;
-import java.util.List;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Spring Data JPA repository for the Synthetic entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface SyntheticRepository extends JpaRepository<Synthetic, Long> {
-    @Query(value = "select * from synthetic where voucherNoType = '' order by voucherNo", nativeQuery = true)
+public interface SyntheticRepository extends JpaRepository<Synthetic, Long>, SyntheticRepositoryCustom {
+    @Query(value = "select * from synthetic where voucherTypeNo = 13 order by voucherNo", nativeQuery = true)
     List<Synthetic> getAccreditativeOrderByVoucherNo();
 }
